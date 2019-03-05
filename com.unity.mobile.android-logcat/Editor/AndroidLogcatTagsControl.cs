7477f079a29b31edd0abe21afdb772e9c9f7eff9
Vincent Zhang <vincent@unity3d.com>
2019-03-05 15:24:45 +0800

    [Tag] Some code cleanup.

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatTagsControl.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatTagsControl.cs
@@ -14,11 +14,11 @@ namespace Unity.Android.Logcat
         private List<string> m_TagNames = new List<string>(new[] { "Filter by all listed tags", "No Filter", null });
 
         [SerializeField]
-        private List<int> m_SelectedTags = new List<int>(new[] { kInvalidTagIndex, kAnyTagIndex, kInvalidTagIndex });
+        private List<int> m_SelectedTags = new List<int>(new[] { kInvalidTagIndex, kNoFilterIndex, kInvalidTagIndex });
 
-        private const byte kFilterByAllTags = 0;
-        private const byte kAnyTagIndex = 1;
-        private const byte kIndexOfFirstValidTag = 3; // Skipt the first 2 options + separator
+        private const byte kAllTagsIndex = 0;
+        private const byte kNoFilterIndex = 1;
+        private const byte kFirstValidTagIndex = 3; // Skip the first 2 options + separator
         private const int kInvalidTagIndex = Int32.MaxValue;
 
         public event Action TagSelectionChanged;
@@ -35,7 +35,7 @@ namespace Unity.Android.Logcat
             // Tag names
             m_TagNames.Add(tag);
 
-            // Indexes
+            // Indices
             m_SelectedTags.Add(kInvalidTagIndex);
 
             if (selected)
@@ -52,20 +52,20 @@ namespace Unity.Android.Logcat
             var newTags = new List<string>(m_TagNames);
             var tagIndex = newTags.IndexOf(tag);
 
-            if (tagIndex < kIndexOfFirstValidTag)
+            if (tagIndex < kFirstValidTagIndex)
                 return false;
 
             if (updateSelection && IsSelected(tagIndex))
             {
-                TagSelected(null, null, tagIndex); // unselect it
-                TagSelected(null, null, kAnyTagIndex); // Select *Any Tag*
+                TagSelected(null, null, tagIndex); // Deselect it
+                TagSelected(null, null, kNoFilterIndex); // Select *No Filter*
             }
 
             newTags.Remove(tag);
 
             m_TagNames = newTags;
 
-            // Selected indexes
+            // Selected indices
             m_SelectedTags.RemoveAt(tagIndex);
 
             return true;
@@ -73,7 +73,7 @@ namespace Unity.Android.Logcat
 
         public string[] GetSelectedTags()
         {
-            if (m_SelectedTags[kAnyTagIndex] == kAnyTagIndex)
+            if (m_SelectedTags[kNoFilterIndex] == kNoFilterIndex)
                 return new string[0];
 
             var selectedTagNames = new List<string>(m_SelectedTags.Count);
@@ -96,42 +96,42 @@ namespace Unity.Android.Logcat
             EditorUtility.DisplayCustomMenuWithSeparators(new Rect(rect.x, rect.y + rect.height, 0, 0), m_TagNames.ToArray(), enabled, separators, m_SelectedTags.ToArray(), TagSelected, null);
         }
 
-        private void TagSelected(object userData, string[] options, int selected)
+        private void TagSelected(object userData, string[] options, int selectedIndex)
         {
-            if (selected == kFilterByAllTags)
+            if (selectedIndex == kAllTagsIndex)
             {
-                // unselect *any tag* and select all of others...s
-                UpdateTagFilterBasedOnAnyTagOption(false);
+                // Deselect *No Filter* and select all others.
+                UpdateTagFilterBasedOnNoFilterOption(false);
             }
-            else if (selected == kAnyTagIndex)
+            else if (selectedIndex == kNoFilterIndex)
             {
-                if (m_SelectedTags[kAnyTagIndex] == kInvalidTagIndex)
+                if (m_SelectedTags[kNoFilterIndex] == kInvalidTagIndex)
                 {
-                    // Select *any tag*, unselect all others
-                    UpdateTagFilterBasedOnAnyTagOption(true);
+                    // Select *No Filter*, deselect all others.
+                    UpdateTagFilterBasedOnNoFilterOption(true);
                 }
                 else
                 {
-                    // Unselect *any tag*, select all others...
-                    UpdateTagFilterBasedOnAnyTagOption(false);
+                    // Deselect *No Filter*, select all others.
+                    UpdateTagFilterBasedOnNoFilterOption(false);
                 }
             }
             else
             {
-                m_SelectedTags[selected] = m_SelectedTags[selected] == kInvalidTagIndex ? selected : kInvalidTagIndex;
-                m_SelectedTags[kAnyTagIndex] = kInvalidTagIndex;
+                m_SelectedTags[selectedIndex] = m_SelectedTags[selectedIndex] == kInvalidTagIndex ? selectedIndex : kInvalidTagIndex;
+                m_SelectedTags[kNoFilterIndex] = kInvalidTagIndex;
             }
 
             if (TagSelectionChanged != null)
                 TagSelectionChanged.Invoke();
         }
 
-        private void UpdateTagFilterBasedOnAnyTagOption(bool selectAnyTagEntry)
+        private void UpdateTagFilterBasedOnNoFilterOption(bool isNoFilterEntrySelected)
         {
-            for (int i = kIndexOfFirstValidTag; i < m_SelectedTags.Count; i++)
-                m_SelectedTags[i] = selectAnyTagEntry ? kInvalidTagIndex : i;
+            for (int i = kFirstValidTagIndex; i < m_SelectedTags.Count; i++)
+                m_SelectedTags[i] = isNoFilterEntrySelected ? kInvalidTagIndex : i;
 
-            m_SelectedTags[kAnyTagIndex] = selectAnyTagEntry ? kAnyTagIndex : kInvalidTagIndex;
+            m_SelectedTags[kNoFilterIndex] = isNoFilterEntrySelected ? kNoFilterIndex : kInvalidTagIndex;
         }
 
         private bool IsSelected(int index)
