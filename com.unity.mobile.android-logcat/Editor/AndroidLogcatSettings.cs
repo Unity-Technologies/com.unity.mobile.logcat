f6427c13bb0418c68281b5707795b2acee3f3fe3
Tomas Dirvanauskas <tomas@unity3d.com>
2019-08-06 12:02:14 +0300

    Further improving Android Logcat settings

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatSettings.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatSettings.cs
@@ -21,11 +21,111 @@ namespace Unity.Android.Logcat
         [SerializeField]
         private int m_MaxMessageCount;
 
-        internal int MaxMessageCount { set { m_MaxMessageCount = value; } get { return m_MaxMessageCount; } }
+        [SerializeField]
+        private Font m_MessageFont;
+
+        [SerializeField]
+        private int m_MessageFontSize;
+
+        [SerializeField]
+        private List<Color> m_MessageColors;
+
+        internal int MaxMessageCount
+        {
+            set
+            {
+                if (m_MaxMessageCount == value)
+                    return;
+                m_MaxMessageCount = value;
+                InvokeOnSettingsChanged();
+            }
+            get
+            {
+                return m_MaxMessageCount;
+            }
+        }
+
+        internal Font MessageFont
+        {
+            set
+            {
+                if (m_MessageFont == value)
+                    return;
+                m_MessageFont = value;
+                InvokeOnSettingsChanged();
+            }
+            get
+            {
+                return m_MessageFont;
+            }
+        }
+
+        internal int MessageFontSize
+        {
+            set
+            {
+                if (m_MessageFontSize == value)
+                    return;
+                m_MessageFontSize = value;
+                InvokeOnSettingsChanged();
+            }
+            get
+            {
+                return m_MessageFontSize;
+            }
+        }
+
+        internal void SetMessageColor(AndroidLogcat.Priority priority, Color color)
+        {
+            // Populate the color list if needed
+            while ((int)priority >= m_MessageColors.Count)
+                m_MessageColors.Add(Color.white);
+
+            if (m_MessageColors[(int)priority] == color)
+                return;
+            m_MessageColors[(int)priority] = color;
+            InvokeOnSettingsChanged();
+        }
+
+        internal Color GetMessageColor(AndroidLogcat.Priority priority)
+        {
+            if ((int)priority < m_MessageColors.Count)
+                return m_MessageColors[(int)priority];
+            return Color.white;
+        }
+
+        internal Action<AndroidLogcatSettings> OnSettingsChanged;
 
         private AndroidLogcatSettings()
         {
+            Reset();
+        }
+
+        internal void Reset()
+        {
             m_MaxMessageCount = 60000;
+            m_MessageFont = (Font)EditorGUIUtility.LoadRequired(UnityEditor.Experimental.EditorResources.fontsPath + "consola.ttf");
+            m_MessageFontSize = 11;
+            if (Enum.GetValues(typeof(AndroidLogcat.Priority)).Length != 6)
+                throw new Exception("Unexpected length of Priority enum.");
+
+            m_MessageColors = new List<Color>(new[]
+            {
+                Color.white,
+                Color.white,
+                Color.white,
+                Color.yellow,
+                Color.red,
+                Color.red
+            });
+            InvokeOnSettingsChanged();
+        }
+
+        private void InvokeOnSettingsChanged()
+        {
+            if (OnSettingsChanged == null)
+                return;
+            OnSettingsChanged(this);
         }
 
         internal static AndroidLogcatSettings Load()
@@ -63,6 +163,7 @@ namespace Unity.Android.Logcat
         {
             public static GUIContent maxMessageCount = new GUIContent("Max Count", "The maximum number of messages.");
             public static GUIContent font = new GUIContent("Font", "Font used for displaying messages");
+            public static GUIContent fontSize = new GUIContent("Font Size");
         }
 
         public AndroidLogcatSettingsProvider(string path, SettingsScope scope)
@@ -73,7 +174,23 @@ namespace Unity.Android.Logcat
             var settings = AndroidLogcatManager.instance.Runtime.Settings;
             EditorGUILayout.LabelField("Messages", EditorStyles.boldLabel);
             settings.MaxMessageCount = EditorGUILayout.IntSlider(Styles.maxMessageCount, settings.MaxMessageCount, 1, 100000);
-            EditorGUILayout.ObjectField(Styles.font, AndroidLogcatStyles.GetFont(), typeof(Font), true);
+            settings.MessageFont = (Font)EditorGUILayout.ObjectField(Styles.font, settings.MessageFont, typeof(Font), true);
+            settings.MessageFontSize = EditorGUILayout.IntSlider(Styles.fontSize, settings.MessageFontSize, 3, 64);
+
+            GUILayout.Space(20);
+            EditorGUILayout.LabelField("Message Colors", EditorStyles.boldLabel);
+            foreach (var p in (AndroidLogcat.Priority[])Enum.GetValues(typeof(AndroidLogcat.Priority)))
+            {
+                settings.SetMessageColor(p, EditorGUILayout.ColorField(p.ToString(), settings.GetMessageColor(p)));
+            }
+
+            GUILayout.Space(20);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Reset"))
+                settings.Reset();
+            GUILayout.Space(5);
+            GUILayout.EndHorizontal();
         }
 
         [SettingsProvider]
