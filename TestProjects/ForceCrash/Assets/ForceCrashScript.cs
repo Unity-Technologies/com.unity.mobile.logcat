1de6818de0f305ea80f362a88f836c2ef1525adf
Tomas Dirvanauskas <tomas@unity3d.com>
2020-01-28 23:29:42 +0200

    Add ForceCrash project

---
+++ TestProjects/ForceCrash/Assets/ForceCrashScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class ForceCrashScript : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical(GUILayout.Height(Screen.height));
+        foreach (var crashType in (ForcedCrashCategory[])Enum.GetValues(typeof(ForcedCrashCategory)))
+        {
+            if (GUILayout.Button(crashType.ToString(), new[] { GUILayout.ExpandHeight(true), GUILayout.Width(Screen.width) }))
+            {
+                Utils.ForceCrash(crashType);
+            }
+        }
+        GUILayout.EndVertical();
+    }
+}
