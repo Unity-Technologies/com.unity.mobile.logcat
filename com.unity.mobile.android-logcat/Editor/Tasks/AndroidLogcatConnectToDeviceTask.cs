1e44e6bec669c0b21307aab1668b5a47ee32ddbc
Tomas Dirvanauskas <tomas@unity3d.com>
2020-04-08 23:17:13 +0300

    Handle exception in AndroidLogcatConnectToDevice

--- com.unity.mobile.android-logcat/Editor/Tasks/AndroidLogcatConnectToDeviceTask.cs
+++ com.unity.mobile.android-logcat/Editor/Tasks/AndroidLogcatConnectToDeviceTask.cs
@@ -26,38 +26,49 @@ namespace Unity.Android.Logcat
     {
         internal static IAndroidLogcatTaskResult Execute(IAndroidLogcatTaskInput input)
         {
-            var workInput = ((AndroidLogcatConnectToDeviceInput)input);
-            var adb = workInput.adb;
-
-            if (adb == null)
-                throw new NullReferenceException("ADB interface has to be valid");
-
-            var ip = workInput.ip;
-            var port = workInput.port;
-            string cmd;
-            if (workInput.setListeningPort)
-            {
-                cmd = "-s " + workInput.deviceId + " tcpip " + port;
-                AndroidLogcatInternalLog.Log("{0} {1}", adb.GetADBPath(), cmd);
-                var resultTCPIP = adb.Run(new[] { cmd }, "Failed to adb tcpip " + port);
-                AndroidLogcatInternalLog.Log(resultTCPIP);
-                var wait = 3000;
-                AndroidLogcatInternalLog.Log("Waiting {0} ms until ADB returns", wait);
-                System.Threading.Thread.Sleep(wait);
-            }
-
-            cmd = "connect " + ip + ":" + port;
-            AndroidLogcatInternalLog.Log("{0} {1}", adb.GetADBPath(), cmd);
-
-            var errorMsg = "Unable to connect to ";
-            var outputMsg = adb.Run(new[] { cmd }, errorMsg + ip + ":" + port);
             var result = new AndroidLogcatConnectToDeviceResult();
-            result.message = outputMsg;
-            result.success = true;
-            if (outputMsg.StartsWith(errorMsg) || outputMsg.StartsWith("failed to connect"))
+
+            try
             {
-                AndroidLogcatInternalLog.Log(outputMsg);
+                var workInput = ((AndroidLogcatConnectToDeviceInput)input);
+                var adb = workInput.adb;
+
+                if (adb == null)
+                    throw new NullReferenceException("ADB interface has to be valid");
+
+                var ip = workInput.ip;
+                var port = workInput.port;
+                string cmd;
+                if (workInput.setListeningPort)
+                {
+                    cmd = "-s " + workInput.deviceId + " tcpip " + port;
+                    AndroidLogcatInternalLog.Log("{0} {1}", adb.GetADBPath(), cmd);
+                    var resultTCPIP = adb.Run(new[] { cmd }, "Failed to adb tcpip " + port);
+                    AndroidLogcatInternalLog.Log(resultTCPIP);
+                    var wait = 3000;
+                    AndroidLogcatInternalLog.Log("Waiting {0} ms until ADB returns", wait);
+                    System.Threading.Thread.Sleep(wait);
+                }
+
+                cmd = "connect " + ip + ":" + port;
+                AndroidLogcatInternalLog.Log("{0} {1}", adb.GetADBPath(), cmd);
+
+                var errorMsg = "Unable to connect to ";
+                var outputMsg = adb.Run(new[] { cmd }, errorMsg + ip + ":" + port);
+
+                result.message = outputMsg;
+                result.success = true;
+                if (outputMsg.StartsWith(errorMsg) || outputMsg.StartsWith("failed to connect"))
+                {
+                    AndroidLogcatInternalLog.Log(outputMsg);
+                    result.success = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                AndroidLogcatInternalLog.Log(ex.Message);
                 result.success = false;
+                result.message = ex.Message;
             }
             return result;
         }
