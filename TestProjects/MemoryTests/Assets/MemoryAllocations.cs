862cadbdd8e30dd9650d199a561df424e5d669ee
Tomas Dirvanauskas <tomas@unity3d.com>
2020-04-15 00:28:59 +0300

    Expand MemoryTest project

--- TestProjects/MemoryTests/Assets/MemoryAllocations.cs
+++ TestProjects/MemoryTests/Assets/MemoryAllocations.cs
@@ -1,11 +1,22 @@
 using System;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MemoryAllocations : MonoBehaviour
 {
+    enum MemoryType
+    {
+        Native,
+        Java,
+        Graphics,
+        Managed
+    }
+
+    const int kOneMB = 1000 * 1000;
     const int kAllocationUnit = 10 * 1000 * 1000;
     const int kAllocationUnityInMb = kAllocationUnit / 1000000;
     struct NativeMemoryData
@@ -15,9 +26,16 @@ public class MemoryAllocations : MonoBehaviour
     }
 
     List<NativeMemoryData> m_NativeAllocations = new List<NativeMemoryData>();
+    List<Texture2D> m_Textures = new List<Texture2D>();
+    List<RenderTexture> m_RenderTextures = new List<RenderTexture>();
+    List<byte[]> m_ManagedMemory = new List<byte[]>();
 
     AndroidJavaObject m_JavaClass;
-    // Start is called before the first frame update
+    MemoryType m_MemoryType;
+
+    GUIContent[] m_ToolbarItems = ((MemoryType[])Enum.GetValues(typeof(MemoryType))).Select(m => new GUIContent(m.ToString())).ToArray();
+
+
     void Start()
     {
         m_JavaClass = new AndroidJavaObject("com.unity3d.player.JavaMemory");
@@ -79,11 +97,8 @@ public class MemoryAllocations : MonoBehaviour
         return m_JavaClass.CallStatic<int>("getTotalMemoryMB");
     }
 
-    void OnGUI()
+    void DoNativeGUI()
     {
-#if !UNITY_EDITOR
-        GUI.matrix = Matrix4x4.Scale(Vector3.one * 5);
-#endif
         GUILayout.Label($"Native Memory allocated {GetAllocatedNativeMemoryMB()} MB");
         if (GUILayout.Button($"Allocate {kAllocationUnityInMb}MB of Native Memory"))
         {
@@ -121,8 +136,10 @@ public class MemoryAllocations : MonoBehaviour
         {
             ClearNativeMemory();
         }
+    }
 
-        GUILayout.Space(20);
+    void DoJavaGUI()
+    {
         GUILayout.Label($"Java Memory allocated {GetAllocatedJavaMemoryMB()} MB");
         if (GUILayout.Button($"Allocate {kAllocationUnityInMb}MB of Java Memory"))
             AllocateJavaMemory(kAllocationUnit);
@@ -143,6 +160,138 @@ public class MemoryAllocations : MonoBehaviour
         if (GUILayout.Button("Clear Java Memory"))
         {
             ClearJavaMemory();
+        }
+    }
+
+    void DoGraphicsGUI()
+    {
+        int width = 1024;
+        int height = 1024;
+        int depth = 16;
+
+        int size = 0;
+        for (int i = 0; i < m_Textures.Count; i++)
+        {
+            size += width * height * 4;
+        }
+
+        int renderTexturesSize = 0;
+        for (int i = 0; i < m_RenderTextures.Count; i++)
+        {
+            renderTexturesSize += width * height * 4 * depth / 8;
+        }
+
+        GUILayout.Label($"Texture2D Memory allocated {size / 1000000} MB");
+        GUILayout.Label($"RenderTexture Memory allocated {renderTexturesSize / 1000000} MB");
+        if (GUILayout.Button($"Allocate Texture2D {4 * width * height / 1000000} MB"))
+        {
+            var texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            var pixels = new Color[width * height];
+            for (int i = 0; i < width * height; i++)
+            {
+                var val = (byte)(i % 255);
+                pixels[i] = new Color32(val, val, val, 255);
+            }
+            texture.SetPixels(pixels);
+            texture.Apply();
+            m_Textures.Add(texture);
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Clear Texture2D Memory"))
+        {
+            m_Textures.Clear();
+            GC.Collect();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        GUILayout.Space(20);
+        if (GUILayout.Button($"Allocate RenderTexture {depth / 8 * 4 * width * height / 1000000} MB"))
+        {
+            var r = new RenderTexture(width, height, depth, RenderTextureFormat.ARGB32);
+            r.Create();
+            var camera = GetComponent<Camera>();
+            camera.targetTexture = r;
+            camera.Render();
+            camera.targetTexture = null;
+
+            m_RenderTextures.Add(r);
+        }
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Clear RenderTexture Memory"))
+        {
+            foreach (var r in m_RenderTextures)
+            {
+                r.Release();
+            }
+            m_RenderTextures.Clear();
+            GC.Collect();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+
+        var rc = GUILayoutUtility.GetRect(new GUIContent(" "), GUIStyle.none);
+        var rcW = 300;
+        var rcH = 150;
+        foreach (var r in m_Textures)
+            GUI.DrawTexture(new Rect(rc.x, rc.y, rcW * 0.5f, rcH), r);
+        foreach (var r in m_RenderTextures)
+            GUI.DrawTexture(new Rect(rc.x + rcW * 0.5f, rc.y, rcW * 0.5f, rcH), r);
+    }
+
+    void DoManagedGUI()
+    {
+        int size = 0;
+        foreach (var m in m_ManagedMemory)
+        {
+            size += m.Length;
+        }
+        GUILayout.Label($"Managed Memory allocated {size / 1000000} MB");
+        if (GUILayout.Button($"Allocate {kAllocationUnityInMb}MB of Managed Memory"))
+        {
+            m_ManagedMemory.Add(new byte[kAllocationUnityInMb * kOneMB]);
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button($"Dirty Managed Memory"))
+        {
+            foreach (var m in m_ManagedMemory)
+            {
+                for (int i = 0; i < m.Length; i++)
+                {
+                    m[i] = 0;
+                }
+            }
+        }
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Clear Managed Memory"))
+        {
+            m_ManagedMemory.Clear();
+            GC.Collect();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+
+    void OnGUI()
+    {
+#if !UNITY_EDITOR
+        GUI.matrix = Matrix4x4.Scale(Vector3.one * 5);
+#endif
+        GUILayout.Space(20);
+        m_MemoryType = (MemoryType)GUILayout.Toolbar((int)m_MemoryType, m_ToolbarItems);
+        switch (m_MemoryType)
+        {
+            case MemoryType.Native: DoNativeGUI(); break;
+            case MemoryType.Java: DoJavaGUI(); break;
+            case MemoryType.Graphics: DoGraphicsGUI(); break;
+            case MemoryType.Managed: DoManagedGUI(); break;
         }
     }
 }
