4c867f647079d6dd11395b3f8c4e84eebc2f9c4a
Tomas Dirvanauskas <tomas@unity3d.com>
2020-06-29 18:53:15 +0300

    Call RefreshPackagesForSerialization

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatProjectSettings.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatProjectSettings.cs
@@ -176,6 +176,7 @@ namespace Unity.Android.Logcat
             };
 
             packages.Add(newPackage);
+            RefreshPackagesForSerialization();
             return newPackage;
         }
 
