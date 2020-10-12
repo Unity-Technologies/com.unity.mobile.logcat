7ad2b46bd60a964e4e22e2a5396e36c414065b8d
Tomas Dirvanauskas <tomas@unity3d.com>
2020-10-12 18:16:29 +0300

    Unity Downloader downloads NDK for us, no need to set it up manually

--- com.unity.mobile.android-logcat/Tests/Editor/AndroidLogcatTestsSetup.cs
+++ com.unity.mobile.android-logcat/Tests/Editor/AndroidLogcatTestsSetup.cs
@@ -9,42 +9,6 @@ using System;
 [InitializeOnLoad]
 public class AndroidLogcatTestsSetup
 {
-    /// <summary>
-    /// Set SKD/NDK for Bokken images
-    /// </summary>
-    static AndroidLogcatTestsSetup()
-    {
-#if UNITY_2019_3_OR_NEWER
-        if (!AndroidBridge.AndroidExtensionsInstalled)
-        {
-            Debug.LogWarning("Android Support not installed, skipping setup.");
-            return;
-        }
-
-        var sdkPath = Environment.GetEnvironmentVariable("ANDROID_SDK_ROOT");
-        if (sdkPath != string.Empty)
-        {
-            AndroidBridge.AndroidExternalToolsSettings.sdkRootPath = sdkPath;
-            Debug.Log($"SDK Path was set from ANDROID_SDK_ROOT = {sdkPath}");
-        }
-        else
-        {
-            Debug.LogWarning($"ANDROID_SDK_ROOT was not set.\nCurrently using SDK from here: {AndroidBridge.AndroidExternalToolsSettings.sdkRootPath}");
-        }
-
-        var ndkPath = Environment.GetEnvironmentVariable("ANDROID_NDK_ROOT");
-        if (ndkPath != string.Empty)
-        {
-            AndroidBridge.AndroidExternalToolsSettings.ndkRootPath = ndkPath;
-            Debug.Log($"SDK Path was set from ANDROID_NDK_ROOT = {ndkPath}");
-        }
-        else
-        {
-            Debug.LogWarning($"ANDROID_NDK_ROOT was not set.\nCurrently using NDK from here: {AndroidBridge.AndroidExternalToolsSettings.ndkRootPath}");
-        }
-#endif
-    }
-
     public static bool AndroidSDKAndNDKAvailable()
     {
 #if UNITY_2019_3_OR_NEWER
