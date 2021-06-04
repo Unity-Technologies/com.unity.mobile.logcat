7119d2d79391e88b4d31377012e015809af0987a
Tomas Dirvanauskas <tomas@unity3d.com>
2021-06-04 12:50:36 +0300

    Add conditionals

---
+++ com.unity.mobile.android-logcat/Tests/Editor/Conditionals.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEditor;
+using UnityEngine.TestTools;
+
+[InitializeOnLoad]
+public class OnLoad
+{
+    static OnLoad()
+    {
+        var value = Environment.GetEnvironmentVariable("ANDROID_DEVICE_IS_AVAILABLE");
+        // Always treat that device is available for tests running locally
+        var androidDeviceIsAvailable = string.IsNullOrEmpty(value) || value != "0";
+        ConditionalIgnoreAttribute.AddConditionalIgnoreMapping(RequiresAndroidDevice.Name, !androidDeviceIsAvailable);
+    }
+}
+
+public class RequiresAndroidDevice : ConditionalIgnoreAttribute
+{
+    internal const string Name = nameof(RequiresAndroidDevice);
+    public RequiresAndroidDevice() :
+        base(Name, "Requires Android Device") {}
+}
