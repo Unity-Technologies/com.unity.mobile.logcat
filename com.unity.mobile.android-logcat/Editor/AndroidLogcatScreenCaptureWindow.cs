104ca096c7874e6f9b59ee6b51be2d02c5f282ee
Tomas Dirvanauskas <tomas@unity3d.com>
2022-03-15 16:58:17 +0200

    Fix video positioning

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatScreenCaptureWindow.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatScreenCaptureWindow.cs
@@ -383,7 +383,7 @@ namespace Unity.Android.Logcat
                     }
                     else
                     {
-                        m_VideoPlayer.DoGUI();
+                        m_VideoPlayer.DoGUI(position);
                         if (m_VideoPlayer.IsPlaying())
                             Repaint();
                     }
