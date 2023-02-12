e8c89d2c487b90133339449181892f4fb14648d5
Tomas Dirvanauskas <tomas@unity3d.com>
2023-02-12 18:27:02 +0200

    Working on parser

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatPacakgeInfoParser.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatPacakgeInfoParser.cs
@@ -1,66 +1,156 @@
 using System;
 using System.Linq;
-using System.IO;
 using System.Text.RegularExpressions;
-using UnityEngine;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Unity.Android.Logcat
 {
     internal class AndroidLogcatPackageInfoParser
     {
+        internal static readonly string FailedKey = "Failed";
         // Note: Using List instead of Dictionary in case of duplicate keys
         List<KeyValuePair<string, string>> m_Entries;
 
-        IReadOnlyList<KeyValuePair<string, string>> Entries => m_Entries;
+        internal IReadOnlyList<KeyValuePair<string, string>> Entries => m_Entries;
 
         internal AndroidLogcatPackageInfoParser(string contents, string packageName)
         {
             m_Entries = new List<KeyValuePair<string, string>>();
 
-            var lines = contents.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries).ToArray();
+            contents = contents.Replace("\r\n", "\n");
 
+            // Note: Keep empty entries, helps us to determine block end
+            var lines = contents.Split(new[] { '\n' }).ToArray();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // Find block start
+                if (!lines[i].Trim().StartsWith($"Package [{packageName}]"))
+                    continue;
+
+                var blockStart = i;
+                var blockEnd = -1;
+                for (var l = blockStart + 1; l < lines.Length; l++)
+                {
+                    blockEnd = l;
+                    // Find block end
+                    if (string.IsNullOrEmpty(lines[blockEnd].Trim()))
+                    {
+                        blockEnd--;
+                        break;
+                    }
+                }
+
+                var length = blockEnd - blockStart + 1;
+                if (length <= 0)
+                    return;
+
+                var blockLines = lines.Skip(i).Take(length).ToArray();
+                ParsePackageInformation(blockLines, packageName);
+                return;
+            }
+        }
+
+        private void ParsePackageInformation(string[] lines, string packageName)
+        {
             if (lines.Length == 0)
                 throw new System.Exception("No package info found");
+
             var regexPackageName = Regex.Escape(packageName);
             var title = new Regex($"Package.*{regexPackageName}.*\\:");
 
             if (!title.Match(lines[0]).Success)
                 throw new Exception($"Expected 'Package [{packageName}] (<id>) :', but got '{lines[0]}'");
 
-            var keyValueRegex = new Regex(@"\s+(?<key>\S+[^\=])\=(?<value>[^\=]\S+)");
-            for (int i = 1; i < lines.Length; i++)
+            var keyValueRegex = new Regex(@"\s+(?<key>\w+)\=(?<value>.*)");
+            for (var i = 1; i < lines.Length;)
             {
                 var l = lines[i];
 
-                // Is it a permissions block
+                // Is it a permissions block?
                 if (l.EndsWith("permissions:"))
                 {
                     var key = l.Trim();
-                    var value = string.Empty;
+                    var values = ParsePermissionBlock(lines, i, out var blockLength);
+                    m_Entries.Add(new KeyValuePair<string, string>(key, values));
+                    i += blockLength;
+                    continue;
+                }
+
+                // Normal entry
+                var result = keyValueRegex.Match(l);
+
+                if (result.Success)
+                {
+                    var key = result.Groups["key"].Value;
+                    var value = result.Groups["value"] == null ? string.Empty : result.Groups["value"].Value;
+
                     m_Entries.Add(new KeyValuePair<string, string>(key, value));
                 }
                 else
                 {
-                    var result = keyValueRegex.Match(l);
-
-                    if (result.Success)
-                    {
-                        var key = result.Groups["key"].Value;
-                        var value = result.Groups["value"].Value;
-
-                        m_Entries.Add(new KeyValuePair<string, string>(key, value));
-                    }
-                    else
-                    {
-                        // Keep failures too
-                        m_Entries.Add(new KeyValuePair<string, string>("Failed", l));
-                    }
+                    // Failed to parse value, add it as well for easier debugging
+                    m_Entries.Add(new KeyValuePair<string, string>(FailedKey, l));
                 }
 
+                i++;
             }
-
         }
 
+        private int GetBlockEnd(string[] lines, int blockStart)
+        {
+            // Figure out block end
+            // For ex.,
+            //  requested permissions:
+            //      android.permission.INTERNET
+            //      android.permission.ACCESS_NETWORK_STATE
+            // if blockStart == 0, blockEnd will be 3
+            var l = lines[blockStart];
+            var indentation = l.TakeWhile(c => char.IsWhiteSpace(c)).Count();
+
+            var blockEnd = blockStart + 1;
+            while (blockEnd < lines.Length)
+            {
+                l = lines[blockEnd];
+                var blockIndentation = l.TakeWhile(c => char.IsWhiteSpace(c)).Count();
+                if (blockIndentation > indentation)
+                    blockEnd++;
+                else
+                    break;
+            }
+
+            return blockEnd - 1;
+        }
+
+        private string ParsePermissionBlock(string[] lines, int permissionsStart, out int blockLength)
+        {
+            blockLength = 1;
+            var value = string.Empty;
+
+            if (permissionsStart + 1 >= lines.Length)
+                return string.Empty;
+
+            var permissionsEnd = GetBlockEnd(lines, permissionsStart);
+
+            // Collect values
+            for (int p = permissionsStart + 1; p <= permissionsEnd; p++)
+            {
+                if (value.Length > 0)
+                    value += ", ";
+                value += lines[p].Trim();
+            }
+            blockLength = permissionsEnd - permissionsStart + 1;
+            return value;
+        }
+
+        internal string GetEntriesString()
+        {
+            var values = new StringBuilder();
+            foreach (var e in Entries)
+            {
+                values.AppendLine($"{e.Key} = {e.Value}");
+            }
+            return values.ToString();
+        }
     }
 }
--- com.unity.mobile.android-logcat/Tests/Editor/AndroidLogcatPackageInfoTests.cs
+++ com.unity.mobile.android-logcat/Tests/Editor/AndroidLogcatPackageInfoTests.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using System.Text.RegularExpressions;
 using Unity.Android.Logcat;
 using System.Linq;
 using System.Text;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 class AndroidLogcatPacakgeInfoTests
 {
+    private void AssertDictionary(Dictionary<string, string> dictionary, string key, string value)
+    {
+        var allKeys = string.Join("\n", dictionary.Keys);
+        Assert.IsTrue(dictionary.ContainsKey(key), $"Key {key} doesn't exist, existing keys:\n{allKeys}");
+
+        Assert.IsTrue(dictionary[key].Equals(value), $"Key = {key}, expected value '{value}', but got '{dictionary[key]}'");
+    }
+
+    private Dictionary<string, string> EntriesToDictionary(AndroidLogcatPackageInfoParser parser)
+    {
+        Assert.That(parser.Entries.Count, Is.GreaterThan(0));
+
+        var failedParsings = new StringBuilder();
+        foreach (var e in parser.Entries)
+        {
+            if (e.Key.Equals(AndroidLogcatPackageInfoParser.FailedKey))
+                failedParsings.AppendLine(e.Value);
+        }
+
+        Assert.IsTrue(failedParsings.Length == 0, $"Failed to parse some values:\n{failedParsings}");
+
+        Dictionary<string, string> entries;
+        try
+        {
+            entries = parser.Entries.ToDictionary(x => x.Key, x => x.Value);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Failed to form dictionary from:\n{parser.GetEntriesString()}", ex);
+        }
+        return entries;
+    }
+
     // adb shell dumpsys package <package information>
     [Test]
     public void CanParsePackageInfo()
     {
         var packageName = "com.DefaultCompany.AndroidEmptyGameActivity";
-        var contents = @$"  Package [{packageName}] (ffc5f27):
+        var contents = @$"  Some other data
+
+  Package [{packageName}] (ffc5f27):
     userId=10198
     pkg=Package{{d78b8ca {packageName}}}
     codePath=/data/app/~~ur3u-ye7dI8GpfIRkeYy7Q==/{packageName}-bzIOds7vr6jRoCaEw1TacQ==
@@ -30,6 +67,7 @@ class AndroidLogcatPacakgeInfoTests
     forceQueryable=false
     queriesPackages=[]
     dataDir=/data/user/0/{packageName}
+    fakeEmptyEntry=
     supportsScreens=[small, medium, large, xlarge, resizeable, anyDensity]
     timeStamp=2022-09-19 15:52:25
     firstInstallTime=2022-09-13 16:14:57
@@ -47,70 +85,84 @@ class AndroidLogcatPacakgeInfoTests
       gids=[3003]
       runtime permissions:
 ";
+        var parser = new AndroidLogcatPackageInfoParser(contents, packageName);
+        var entries = EntriesToDictionary(parser);
 
-        var errors = new StringBuilder();
-        var lines = contents.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries).Select(s => s.TrimStart()).ToArray();
+        AssertDictionary(entries, "pkg", $"Package{{d78b8ca {packageName}}}");
+        AssertDictionary(entries, "codePath", $"/data/app/~~ur3u-ye7dI8GpfIRkeYy7Q==/{packageName}-bzIOds7vr6jRoCaEw1TacQ==");
+        AssertDictionary(entries, "flags", "[ DEBUGGABLE HAS_CODE ALLOW_CLEAR_USER_DATA TEST_ONLY ALLOW_BACKUP ]");
+        AssertDictionary(entries, "firstInstallTime", $"2022-09-13 16:14:57");
+        AssertDictionary(entries, "fakeEmptyEntry", string.Empty);
+        AssertDictionary(entries, "signatures", $"PackageSignatures{{af06a3b version:2, signatures:[ca97cd1f], past signatures:[]}}");
+    }
 
-        if (lines.Length == 0)
-            throw new System.Exception("No package info found");
-        var regexPackageName = Regex.Escape(packageName);
-        var title = new Regex($"Package.*{regexPackageName}.*\\:");
+    [Test]
+    public void CanParseEmpty()
+    {
+        var packageName = "com.dummy.dummy";
+        var permissionsEndWithEndLine = @$"
+  Package [{packageName}] (ffc5f27):";
+        var parser = new AndroidLogcatPackageInfoParser(permissionsEndWithEndLine, packageName);
+        Assert.AreEqual(0, parser.Entries.Count);
+    }
 
-        if (!title.Match(lines[0]).Success)
-            throw new System.Exception($"Expected 'Package [<package_name>] (<id>) :', but got '{lines[0]}'");
+    [Test]
+    public void CanParsePermissionsWithEndLine()
+    {
+        var packageName = "com.dummy.dummy";
+        var permissionsEndWithEndLine = @$"
+  Package [{packageName}] (ffc5f27):
+    requested permissions:
+      android.permission.INTERNET
+      android.permission.ACCESS_NETWORK_STATE";
 
-        var keyValueRegex = new Regex(@"(?<key>\S+)\=(?<value>.*)");
-        for (int i = 1; i < lines.Length; i++)
-        {
-            var l = lines[i];
+        var parser = new AndroidLogcatPackageInfoParser(permissionsEndWithEndLine, packageName);
+        var entries = EntriesToDictionary(parser);
 
-            // Check if next lines are list
-            while (l.EndsWith("permissions:"))
-            {
-                UnityEngine.Debug.Log($"{l}");
-                i++;
-                // List permissions
-                while (i < lines.Length)
-                {
-                    var entry = lines[i];
-                    if (entry.StartsWith("android.permission"))
-                    {
-                        UnityEngine.Debug.Log($"P-->   {entry}");
-                        i++;
-                    }
-                    else
-                    {
-                        l = lines[i];
-                        break;
-                    }
-                }
+        AssertDictionary(entries, "requested permissions:", $"android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE");
+    }
 
-                if (i >= lines.Length)
-                    break;
-            }
+    [Test]
+    public void CanParsePermissionsWithExtraLine()
+    {
+        var packageName = "com.dummy.dummy";
+        var permissionsEndWithExtraLine = @$"
+  Package [{packageName}] (ffc5f27):
+    requested permissions:
+      android.permission.INTERNET
+      android.permission.ACCESS_NETWORK_STATE
+";
+        var parser = new AndroidLogcatPackageInfoParser(permissionsEndWithExtraLine, packageName);
+        var entries = EntriesToDictionary(parser);
 
-            if (i >= lines.Length)
-                break;
+        AssertDictionary(entries, "requested permissions:", $"android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE");
+    }
 
-            var result = keyValueRegex.Match(l);
+    [Test]
+    public void CanParsePermissionsEmpty()
+    {
+        var packageName = "com.dummy.dummy";
+        var permissionsEndWithExtraLine = @$"
+  Package [{packageName}] (ffc5f27):
+    requested permissions:";
 
-            if (result.Success)
-            {
-                var key = result.Groups["key"];
-                var value = result.Groups["value"];
+        var parser = new AndroidLogcatPackageInfoParser(permissionsEndWithExtraLine, packageName);
+        var entries = EntriesToDictionary(parser);
 
-                UnityEngine.Debug.Log($"{key}={value}");
-                continue;
-            }
+        AssertDictionary(entries, "requested permissions:", string.Empty);
+    }
 
+    [Test]
+    public void CanParsePermissionsEmptyWithExtraLine()
+    {
+        var packageName = "com.dummy.dummy";
+        var permissionsEndWithExtraLine = @$"
+  Package [{packageName}] (ffc5f27):
+    requested permissions:
+";
+        var parser = new AndroidLogcatPackageInfoParser(permissionsEndWithExtraLine, packageName);
+        var entries = EntriesToDictionary(parser);
 
-            errors.AppendLine($"Failed to resolve {l}");
-        }
-
-        if (errors.Length > 0)
-        {
-            UnityEngine.Debug.LogError($"Found errors:\n{errors.ToString()}\n\nin\n{contents}");
-        }
-
+        AssertDictionary(entries, "requested permissions:", string.Empty);
     }
 }
