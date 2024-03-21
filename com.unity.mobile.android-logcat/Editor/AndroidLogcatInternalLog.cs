95f4541ad61991d3ea6a992b02504fb6b46e7ebe
Tomas Dirvanauskas <tomas@unity3d.com>
2024-03-21 17:42:53 +0200

    WIP internal log

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatInternalLog.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatInternalLog.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 using System.Text;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace Unity.Android.Logcat
 {
@@ -15,14 +16,14 @@ namespace Unity.Android.Logcat
             public string Entry { get; set; }
         }
 
-        const int kMaxInternalLogBuffer = 15000;
+        const int kMaxInternalLogMessages = 10000;
         static AndroidLogcatInternalLog ms_Instance = null;
-        static StringBuilder ms_LogEntries = new StringBuilder();
-        static List<InternalLogEntry> ms_LogEntries2 = new List<InternalLogEntry>();
+        static List<InternalLogEntry> ms_LogEntries = new List<InternalLogEntry>();
 
         Vector2 m_ScrollPosition = Vector2.zero;
         Rect m_ScrollArea;
         float m_MaxEntryWidth;
+        static bool s_RecalculateMaxEntryWidth;
 
         public static void ShowLog(bool immediate)
         {
@@ -49,35 +50,27 @@ namespace Unity.Android.Logcat
             var timedMessage = AndroidLogcatDispatcher.isMainThread ? "[MainThread]" : "[WorkThread] ";
             timedMessage += DateTime.Now.ToString("HH:mm:ss.ffff") + " " + message;
 
-            lock (ms_LogEntries2)
+            lock (ms_LogEntries)
             {
                 var rawEntries = timedMessage.Split(new[] { '\n' });
                 if (rawEntries.Length == 0)
                     return;
 
-                var start = ms_LogEntries2.Count;
-                ms_LogEntries2.AddRange(new InternalLogEntry[rawEntries.Length]);
+                var start = ms_LogEntries.Count;
+                ms_LogEntries.AddRange(new InternalLogEntry[rawEntries.Length]);
                 var entries = new InternalLogEntry[rawEntries.Length];
                 for (int i = 0; i < entries.Length; i++)
                 {
-                    ms_LogEntries2[start + i] = new InternalLogEntry()
+                    ms_LogEntries[start + i] = new InternalLogEntry()
                     {
                         Entry = rawEntries[i]
                     };
                 }
                 
-            }
-            lock (ms_LogEntries)
-            {
-                ms_LogEntries.AppendLine(timedMessage);
+                if (ms_LogEntries.Count > kMaxInternalLogMessages)
+                    ms_LogEntries.RemoveRange(0, ms_LogEntries.Count - kMaxInternalLogMessages);
 
-                const int MaxTriesToStripBuffer = 30;
-                for (int i = 0; i < MaxTriesToStripBuffer; i++)
-                {
-                    if (ms_LogEntries.Length < kMaxInternalLogBuffer)
-                        break;
-                    ms_LogEntries.Remove(0, Convert.ToString(ms_LogEntries).Split('\n').FirstOrDefault().Length + 1);
-                }  
+                s_RecalculateMaxEntryWidth = true;
             }
 
             Console.WriteLine("[Logcat] " + timedMessage);
@@ -103,56 +96,29 @@ namespace Unity.Android.Logcat
                 return;
             }
 
+            DoEntriesGUI();
 
             GUILayout.BeginHorizontal();
-            int count;
-            lock (ms_LogEntries)
-            {
-                count = ms_LogEntries2.Count;
-            }
-
-            GUILayout.Label("Entries: " + count);
+            GUILayout.Label($"Entries: {ms_LogEntries.Count} / {kMaxInternalLogMessages}");
             if (GUILayout.Button("Clear"))
             {
                 lock (ms_LogEntries)
                 {
-                    ms_LogEntries = new StringBuilder();
-                    ms_LogEntries2.Clear();
+                    ms_LogEntries.Clear();
+                    s_RecalculateMaxEntryWidth = true;
                 }
             }
-
+            if (Unsupported.IsDeveloperMode())
+                DoDebuggingGUI();
             GUILayout.EndHorizontal();
+            GUI.Box(GUILayoutUtility.GetLastRect(), GUIContent.none, EditorStyles.helpBox);
 
             var logHeight = AndroidLogcatStyles.internalLogStyle.fixedHeight;
             var screenWidth = Screen.width;
             var e = Event.current;
 
-            if (e.type == EventType.Repaint)
-            {
-                if (e.type == EventType.Repaint)
-                {
-                    var startIdx = (int)(m_ScrollPosition.y / logHeight);
-                    var displayCount = m_ScrollArea.height / logHeight;
-                    var style = AndroidLogcatStyles.internalLogStyle;
-                    m_MaxEntryWidth = 0;
-                    for (int i = 0; i < ms_LogEntries2.Count; i++)
-                    {
-                        
-                        m_MaxEntryWidth = Mathf.Max(m_MaxEntryWidth, style.CalcSize(new GUIContent(ms_LogEntries2[i].Entry)).x);
-                    }
-                    for (int i = 0; i < ms_LogEntries2.Count; i++)
-                    {
-                        if (i < startIdx || i >= startIdx + displayCount)
-                            continue;
-                        var ee = ms_LogEntries2[i];
-                        var eerc = new Rect(-m_ScrollPosition.x, m_ScrollArea.y + i * logHeight - m_ScrollPosition.y, m_MaxEntryWidth, logHeight);
-                            style.Draw(eerc, ee.Entry, false, false, ee.Selected, false);
-                    }
-                }
-            }
-
             m_ScrollPosition = GUILayout.BeginScrollView(m_ScrollPosition, true, true);
-            GUILayoutUtility.GetRect(m_MaxEntryWidth, ms_LogEntries2.Count * logHeight);
+            GUILayoutUtility.GetRect(m_MaxEntryWidth, ms_LogEntries.Count * logHeight);
             GUILayout.EndScrollView();
 
             if (e.type == EventType.Repaint)
@@ -165,18 +131,71 @@ namespace Unity.Android.Logcat
             {
                 if (e.button == 0)
                 {
-                    var idx = (int)((e.mousePosition.y - m_ScrollArea.y + m_ScrollPosition.y) / (float)AndroidLogcatStyles.internalLogStyle.fixedHeight);
-                    if (idx < ms_LogEntries2.Count)
+                    var idx = (int)((e.mousePosition.y - m_ScrollArea.y + (int)(m_ScrollPosition.y / logHeight) * logHeight) / (float)AndroidLogcatStyles.internalLogStyle.fixedHeight);
+                    if (idx < ms_LogEntries.Count)
                     {
-                        ms_LogEntries2[idx].Selected = !ms_LogEntries2[idx].Selected;
+                        if (!e.HasCtrlOrCmdModifier())
+                        {
+                            foreach (var ee in ms_LogEntries)
+                                ee.Selected = false;
+                        }
+
+                        ms_LogEntries[idx].Selected = !ms_LogEntries[idx].Selected;
                         Repaint();
                     }
                 }
                 else if (e.button == 1)
                 {
-                    var menuItems = new[] { new GUIContent("Copy All") };
+                    var menuItems = new[] { new GUIContent("Copy"), new GUIContent("Select All") };
                     EditorUtility.DisplayCustomMenu(new Rect(e.mousePosition.x, e.mousePosition.y, 0, 0),
                         menuItems.ToArray(), -1, MenuSelection, null);
+                }
+            }
+        }
+
+        private void DoEntriesGUI()
+        {
+            if (Event.current.type != EventType.Repaint)
+                return;
+
+            var logHeight = AndroidLogcatStyles.internalLogStyle.fixedHeight;
+            var startIdx = (int)(m_ScrollPosition.y / logHeight);
+            var displayCount = m_ScrollArea.height / logHeight;
+            var style = AndroidLogcatStyles.internalLogStyle;
+
+            if (s_RecalculateMaxEntryWidth)
+            {
+                s_RecalculateMaxEntryWidth = false;
+                m_MaxEntryWidth = 0;
+                for (int i = 0; i < ms_LogEntries.Count; i++)
+                {
+                    m_MaxEntryWidth = Mathf.Max(m_MaxEntryWidth, style.CalcSize(new GUIContent(ms_LogEntries[i].Entry)).x);
+                }
+            }
+
+            for (int i = 0; i < ms_LogEntries.Count; i++)
+            {
+                if (i < startIdx || i >= startIdx + displayCount)
+                    continue;
+                var ee = ms_LogEntries[i];
+                var eerc = new Rect(-m_ScrollPosition.x, m_ScrollArea.y + (i - startIdx) * logHeight, m_MaxEntryWidth, logHeight);
+                style.Draw(eerc, ee.Entry, false, false, ee.Selected, false);
+            }
+        }
+
+        void DoDebuggingGUI()
+        {
+            var entryCount = 1000;
+            GUILayout.Label("Debugging Options:");
+            if (GUILayout.Button($"Add {entryCount} entries"))
+            {
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                var random = new System.Random(0);
+                for (int i = 0; i < entryCount; i++)
+                {
+                    var randomText = new String(Enumerable.Repeat(chars, UnityEngine.Random.Range(50, 100))
+                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                    Log($"[{i}] {randomText}");
                 }
             }
         }
@@ -185,9 +204,22 @@ namespace Unity.Android.Logcat
         {
             switch (selected)
             {
-                // Copy All
+                // Copy
                 case 0:
-                    EditorGUIUtility.systemCopyBuffer = ms_LogEntries.ToString();
+                    var builder = new StringBuilder();
+                    foreach (var ee in ms_LogEntries)
+                    {
+                        if (!ee.Selected)
+                            continue;
+                        builder.AppendLine(ee.Entry);
+                    }
+
+                    EditorGUIUtility.systemCopyBuffer = builder.ToString();
+                    break;
+                // Select All
+                case 1:
+                    foreach (var ee in ms_LogEntries)
+                        ee.Selected = true;
                     break;
             }
         }
