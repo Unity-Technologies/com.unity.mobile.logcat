9a8ed5a0bdb619df90ee4843e8f98740a4f383ba
Tomas Dirvanauskas <tomas@unity3d.com>
2024-03-21 20:29:51 +0200

    Export fast list view out of internal log

--- com.unity.mobile.android-logcat/Editor/AndroidLogcatInternalLog.cs
+++ com.unity.mobile.android-logcat/Editor/AndroidLogcatInternalLog.cs
@@ -4,26 +4,15 @@ using System.Linq;
 using UnityEngine;
 using System.Text;
 using UnityEditor;
-using UnityEngine.UIElements;
 
 namespace Unity.Android.Logcat
 {
     internal class AndroidLogcatInternalLog : EditorWindow
     {
-        class InternalLogEntry
-        {
-            public bool Selected { get; set; }
-            public string Entry { get; set; }
-        }
-
-        const int kMaxInternalLogMessages = 10000;
+        const int MaxInternalLogMessages = 10000;
         static AndroidLogcatInternalLog ms_Instance = null;
-        static List<InternalLogEntry> ms_LogEntries = new List<InternalLogEntry>();
 
-        Vector2 m_ScrollPosition = Vector2.zero;
-        Rect m_ScrollArea;
-        float m_MaxEntryWidth;
-        static bool s_RecalculateMaxEntryWidth;
+        AndroidLogcatFastListView m_ListView;
 
         public static void ShowLog(bool immediate)
         {
@@ -50,33 +39,17 @@ namespace Unity.Android.Logcat
             var timedMessage = AndroidLogcatDispatcher.isMainThread ? "[MainThread]" : "[WorkThread] ";
             timedMessage += DateTime.Now.ToString("HH:mm:ss.ffff") + " " + message;
 
-            lock (ms_LogEntries)
-            {
-                var rawEntries = timedMessage.Split(new[] { '\n' });
-                if (rawEntries.Length == 0)
-                    return;
 
-                var start = ms_LogEntries.Count;
-                ms_LogEntries.AddRange(new InternalLogEntry[rawEntries.Length]);
-                var entries = new InternalLogEntry[rawEntries.Length];
-                for (int i = 0; i < entries.Length; i++)
-                {
-                    ms_LogEntries[start + i] = new InternalLogEntry()
-                    {
-                        Entry = rawEntries[i]
-                    };
-                }
-                
-                if (ms_LogEntries.Count > kMaxInternalLogMessages)
-                    ms_LogEntries.RemoveRange(0, ms_LogEntries.Count - kMaxInternalLogMessages);
+            var rawEntries = timedMessage.Split(new[] { '\n' });
+            if (rawEntries.Length == 0)
+                return;
 
-                s_RecalculateMaxEntryWidth = true;
-            }
+            if (ms_Instance != null)
+                ms_Instance.m_ListView.AddEntries(rawEntries);
 
             Console.WriteLine("[Logcat] " + timedMessage);
             if (AndroidLogcatDispatcher.isMainThread && ms_Instance != null)
             {
-                ms_Instance.m_ScrollPosition = new Vector2(ms_Instance.m_ScrollPosition.x, float.MaxValue);
                 ms_Instance.Repaint();
             }
         }
@@ -86,9 +59,10 @@ namespace Unity.Android.Logcat
             if (!AndroidBridge.AndroidExtensionsInstalled)
                 return;
             ms_Instance = this;
+            m_ListView = new AndroidLogcatFastListView(() => AndroidLogcatStyles.internalLogStyle, MaxInternalLogMessages);
         }
 
-        public void OnGUI()
+        internal void OnGUI()
         {
             if (!AndroidBridge.AndroidExtensionsInstalled)
             {
@@ -96,91 +70,19 @@ namespace Unity.Android.Logcat
                 return;
             }
 
-            DoEntriesGUI();
-
             GUILayout.BeginHorizontal();
-            GUILayout.Label($"Entries: {ms_LogEntries.Count} / {kMaxInternalLogMessages}");
+            GUILayout.Label($"Entries: {m_ListView.Entries.Count()} / {MaxInternalLogMessages}");
             if (GUILayout.Button("Clear"))
             {
-                lock (ms_LogEntries)
-                {
-                    ms_LogEntries.Clear();
-                    s_RecalculateMaxEntryWidth = true;
-                }
+                m_ListView.ClearEntries();
             }
             if (Unsupported.IsDeveloperMode())
                 DoDebuggingGUI();
             GUILayout.EndHorizontal();
             GUI.Box(GUILayoutUtility.GetLastRect(), GUIContent.none, EditorStyles.helpBox);
 
-            var logHeight = AndroidLogcatStyles.internalLogStyle.fixedHeight;
-            var screenWidth = Screen.width;
-            var e = Event.current;
-
-            m_ScrollPosition = GUILayout.BeginScrollView(m_ScrollPosition, true, true);
-            GUILayoutUtility.GetRect(m_MaxEntryWidth, ms_LogEntries.Count * logHeight);
-            GUILayout.EndScrollView();
-
-            if (e.type == EventType.Repaint)
-            {
-                m_ScrollArea = GUILayoutUtility.GetLastRect();
-            }
-
-
-            if (e.type == EventType.MouseDown)
-            {
-                if (e.button == 0)
-                {
-                    var idx = (int)((e.mousePosition.y - m_ScrollArea.y + (int)(m_ScrollPosition.y / logHeight) * logHeight) / (float)AndroidLogcatStyles.internalLogStyle.fixedHeight);
-                    if (idx < ms_LogEntries.Count)
-                    {
-                        if (!e.HasCtrlOrCmdModifier())
-                        {
-                            foreach (var ee in ms_LogEntries)
-                                ee.Selected = false;
-                        }
-
-                        ms_LogEntries[idx].Selected = !ms_LogEntries[idx].Selected;
-                        Repaint();
-                    }
-                }
-                else if (e.button == 1)
-                {
-                    var menuItems = new[] { new GUIContent("Copy"), new GUIContent("Select All") };
-                    EditorUtility.DisplayCustomMenu(new Rect(e.mousePosition.x, e.mousePosition.y, 0, 0),
-                        menuItems.ToArray(), -1, MenuSelection, null);
-                }
-            }
-        }
-
-        private void DoEntriesGUI()
-        {
-            if (Event.current.type != EventType.Repaint)
-                return;
-
-            var logHeight = AndroidLogcatStyles.internalLogStyle.fixedHeight;
-            var startIdx = (int)(m_ScrollPosition.y / logHeight);
-            var displayCount = m_ScrollArea.height / logHeight;
-            var style = AndroidLogcatStyles.internalLogStyle;
-
-            if (s_RecalculateMaxEntryWidth)
-            {
-                s_RecalculateMaxEntryWidth = false;
-                m_MaxEntryWidth = 0;
-                for (int i = 0; i < ms_LogEntries.Count; i++)
-                {
-                    m_MaxEntryWidth = Mathf.Max(m_MaxEntryWidth, style.CalcSize(new GUIContent(ms_LogEntries[i].Entry)).x);
-                }
-            }
-
-            for (int i = 0; i < ms_LogEntries.Count; i++)
-            {
-                if (i < startIdx || i >= startIdx + displayCount)
-                    continue;
-                var ee = ms_LogEntries[i];
-                var eerc = new Rect(-m_ScrollPosition.x, m_ScrollArea.y + (i - startIdx) * logHeight, m_MaxEntryWidth, logHeight);
-                style.Draw(eerc, ee.Entry, false, false, ee.Selected, false);
-            }
+            if (m_ListView.OnGUI())
+                Repaint();
         }
 
         void DoDebuggingGUI()
@@ -197,30 +99,6 @@ namespace Unity.Android.Logcat
                         .Select(s => s[random.Next(s.Length)]).ToArray());
                     Log($"[{i}] {randomText}");
                 }
-            }
-        }
-
-        private void MenuSelection(object userData, string[] options, int selected)
-        {
-            switch (selected)
-            {
-                // Copy
-                case 0:
-                    var builder = new StringBuilder();
-                    foreach (var ee in ms_LogEntries)
-                    {
-                        if (!ee.Selected)
-                            continue;
-                        builder.AppendLine(ee.Entry);
-                    }
-
-                    EditorGUIUtility.systemCopyBuffer = builder.ToString();
-                    break;
-                // Select All
-                case 1:
-                    foreach (var ee in ms_LogEntries)
-                        ee.Selected = true;
-                    break;
             }
         }
     }
---
+++ com.unity.mobile.android-logcat/Editor/UI/AndroidLogcatFastListView.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.Android.Logcat
+{
+    /// <summary>
+    /// Fast way of drawing List view via IMGUI
+    /// We only render visible items, making it possible to have 10,000 or more items without significant performance penalty
+    /// </summary>
+    internal class AndroidLogcatFastListView
+    {
+        internal class Entry
+        {
+            public bool Selected { get; set; }
+            public string Value { get; set; }
+        }
+
+        readonly Func<GUIStyle> GetStyle;
+        readonly uint m_MaxEntries;
+
+        List<Entry> m_LogEntries = new List<Entry>();
+        Vector2 m_ScrollPosition = Vector2.zero;
+        Rect m_ScrollArea;
+        float m_MaxEntryWidth;
+        bool m_RecalculateMaxEntryWidth;
+
+        internal IReadOnlyList<Entry> Entries => m_LogEntries;
+
+        internal AndroidLogcatFastListView(Func<GUIStyle> getStyle, uint maxEntries)
+        {
+            GetStyle = getStyle;
+            m_MaxEntries = maxEntries;
+        }
+
+        /// <summary>
+        /// Add new entries, can be called from different threads
+        /// </summary>
+        /// <param name="newEntries"></param>
+        internal void AddEntries(string[] newEntries)
+        {
+            lock (m_LogEntries)
+            {
+                var start = m_LogEntries.Count;
+                m_LogEntries.AddRange(new Entry[newEntries.Length]);
+                var entries = new Entry[newEntries.Length];
+                for (int i = 0; i < entries.Length; i++)
+                {
+                    m_LogEntries[start + i] = new Entry()
+                    {
+                        Value = newEntries[i]
+                    };
+                }
+
+                if (m_LogEntries.Count > m_MaxEntries)
+                    m_LogEntries.RemoveRange(0, m_LogEntries.Count - (int)m_MaxEntries);
+                m_RecalculateMaxEntryWidth = true;
+            }
+        }
+
+        internal void ClearEntries()
+        {
+            lock (m_LogEntries)
+            {
+                m_LogEntries.Clear();
+                m_RecalculateMaxEntryWidth = true;
+            }
+        }
+
+        // Since we're not drawing all entries, we need to calculate max width, so scroll view can be sized correctly
+        private void RecalculateEntriesMaxWidthIfNeeded()
+        {
+            if (!m_RecalculateMaxEntryWidth)
+                return;
+            m_RecalculateMaxEntryWidth = false;
+            m_MaxEntryWidth = 0;
+            for (int i = 0; i < m_LogEntries.Count; i++)
+            {
+                m_MaxEntryWidth = Mathf.Max(m_MaxEntryWidth, GetStyle().CalcSize(new GUIContent(m_LogEntries[i].Value)).x);
+            }
+        }
+
+        internal bool OnGUI()
+        {
+            RecalculateEntriesMaxWidthIfNeeded();
+
+            DoEntriesGUI();
+
+            var logHeight = GetStyle().fixedHeight;
+            var e = Event.current;
+
+            m_ScrollPosition = GUILayout.BeginScrollView(m_ScrollPosition, true, true);
+            GUILayoutUtility.GetRect(m_MaxEntryWidth, m_LogEntries.Count * logHeight);
+            GUILayout.EndScrollView();
+
+            if (e.type == EventType.Repaint)
+                m_ScrollArea = GUILayoutUtility.GetLastRect();
+
+            if (HandleMouseAndKeyboardControls())
+                return true;
+
+            return false;
+        }
+
+        private void DoEntriesGUI()
+        {
+            if (Event.current.type != EventType.Repaint)
+                return;
+
+            var logHeight = GetStyle().fixedHeight;
+            var startIdx = (int)(m_ScrollPosition.y / logHeight);
+            var displayCount = m_ScrollArea.height / logHeight;
+
+            for (int i = 0; i < m_LogEntries.Count; i++)
+            {
+                if (i < startIdx || i >= startIdx + displayCount)
+                    continue;
+                var entry = m_LogEntries[i];
+                var rc = new Rect(-m_ScrollPosition.x, m_ScrollArea.y + (i - startIdx) * logHeight, m_MaxEntryWidth, logHeight);
+                GetStyle().Draw(rc, entry.Value, false, false, entry.Selected, false);
+            }
+        }
+
+        private bool HandleMouseAndKeyboardControls()
+        {
+            var e = Event.current;
+            if (e.type != EventType.MouseDown)
+                return false;
+
+            if (e.button == 0)
+            {
+                var idx = (int)((e.mousePosition.y - m_ScrollArea.y + (int)(m_ScrollPosition.y / GetStyle().fixedHeight) * GetStyle().fixedHeight) / (float)GetStyle().fixedHeight);
+                if (idx < m_LogEntries.Count)
+                {
+                    if (!e.HasCtrlOrCmdModifier())
+                    {
+                        foreach (var entry in m_LogEntries)
+                            entry.Selected = false;
+                    }
+
+                    m_LogEntries[idx].Selected = !m_LogEntries[idx].Selected;
+                    return true;
+                }
+            }
+            else if (e.button == 1)
+            {
+                var menuItems = new[] { new GUIContent("Copy"), new GUIContent("Select All") };
+                EditorUtility.DisplayCustomMenu(new Rect(e.mousePosition.x, e.mousePosition.y, 0, 0),
+                    menuItems.ToArray(), -1, MenuSelection, null);
+            }
+
+            return false;
+        }
+
+        private void MenuSelection(object userData, string[] options, int selected)
+        {
+            switch (selected)
+            {
+                // Copy
+                case 0:
+                    var builder = new StringBuilder();
+                    foreach (var entry in m_LogEntries)
+                    {
+                        if (!entry.Selected)
+                            continue;
+                        builder.AppendLine(entry.Value);
+                    }
+
+                    EditorGUIUtility.systemCopyBuffer = builder.ToString();
+                    break;
+                // Select All
+                case 1:
+                    foreach (var entry in m_LogEntries)
+                        entry.Selected = true;
+                    break;
+            }
+        }
+    }
+}
