2004175248f5b92f57ce5b85e26feb4c7113a5f5
Tomas <tomas@unity3d.com>
2025-04-08 15:46:08 +0300

    Unity.Mobile.AndroidLogcat.EditorTests -> Unity.Mobile.AndroidLogcat.Editor.Tests

--- com.unity.mobile.android-logcat/Editor/AssemblyInfo.cs
+++ com.unity.mobile.android-logcat/Editor/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.EditorTests")]
+[assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.Integration.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.EditorPerformanceTests")]
 [assembly: InternalsVisibleTo("Unity.Mobile.DeviceExplorer.Editor")]
