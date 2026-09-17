db254ab9c5a0fcd59af45f728736df20690f0f42
Tomas <tomas@unity3d.com>
2026-09-17 12:30:00 +0300

    Revert "Don't use UNITY_7000_0_OR_NEWER define, since CI disallows it"

--- com.unity.mobile.android-logcat/Tests/Editor/AndroidLogcatNetTests.cs
+++ com.unity.mobile.android-logcat/Tests/Editor/AndroidLogcatNetTests.cs
@@ -8,7 +8,6 @@ using System.Text.RegularExpressions;
 using Unity.Android.Logcat;
 using UnityEditor;
 using UnityEditor.Compilation;
-using UnityEngine;
 
 class AndroidLogcatNetTests
 {
@@ -26,20 +25,6 @@ class AndroidLogcatNetTests
         var logcatAssembly = CompilationPipeline.GetAssemblies(AssembliesType.Editor).FirstOrDefault(a => a.name.Equals("Unity.Mobile.AndroidLogcat.Editor"));
         Assert.IsNotNull(logcatAssembly, "Failed to find Android Logcat assembly");
         return logcatAssembly;
-    }
-
-    static bool IsUnity7OrNewer()
-    {
-        var versionString = Application.unityVersion;
-
-        if (string.IsNullOrEmpty(versionString))
-            return false;
-
-        var majorEnd = versionString.IndexOf('.');
-        if (majorEnd <= 0)
-            return false;
-
-        return int.TryParse(versionString.AsSpan(0, majorEnd), out var majorVersion) && majorVersion >= 7000;
     }
 
     /// <summary>
@@ -66,20 +51,20 @@ class AndroidLogcatNetTests
             "UnityEditor.CoreModule"
         });
 
-        if (IsUnity7OrNewer())
+#if UNITY_7000_0_OR_NEWER
+        expectedReferences.AddRange(new[]
         {
-            expectedReferences.AddRange(new[]
-            {
-                "Unity.Scripting",
-                "UnityEngine.ScriptingModule",
-                "UnityEngine.UICommonModule",
-                "UnityEditor.Android.Extensions"
-            });
-        }
+            "Unity.Scripting",
+            "UnityEngine.ScriptingModule",
+            "UnityEngine.UICommonModule",
+            "UnityEditor.Android.Extensions",
+        });
+#endif
 
         var referencedCount = expectedReferences.ToDictionary(s => s, s => 0);
 
         // ReflectionOnlyLoadFrom is unsupported on CoreCLR; inspect the loaded assembly instead.
+
         var references = typeof(AndroidLogcatConsoleWindow).Assembly.GetReferencedAssemblies().Select(a => a.Name);
         var errors = new StringBuilder();
         Console.WriteLine($"Logcat package references:\n{string.Join("\n", references)}");
